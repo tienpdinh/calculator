a362f48931191f6173df4383779a2474e5a46512
Tien Dinh <tiendinhphuc@gmail.com>
2020-03-09 14:25:35 -0500

    Updates

---
+++ precache-manifest.67d9e01a9d6cba0588a8c7363e5a8f9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdb6baec4764588684fc186da43c500",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7e06134c826f721b3fec",
+    "url": "/calculator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0b6f7262cdb3b092135d",
+    "url": "/calculator/static/css/main.75aa2826.chunk.css"
+  },
+  {
+    "revision": "7e06134c826f721b3fec",
+    "url": "/calculator/static/js/2.54accc97.chunk.js"
+  },
+  {
+    "revision": "d68bd37f2996645b1bb3e9deceae2beb",
+    "url": "/calculator/static/js/2.54accc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b6f7262cdb3b092135d",
+    "url": "/calculator/static/js/main.2e527b69.chunk.js"
+  },
+  {
+    "revision": "69d26c5c593e30ac3b22",
+    "url": "/calculator/static/js/runtime-main.0a7dd0a5.js"
+  }
+]);
